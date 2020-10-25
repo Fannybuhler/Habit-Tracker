1ace6fee3ad97fac52dd5ed09ae7b85fb3a87b9c
Fanny Buhlér <fanny.buhler1@gmail.com>
2020-10-25 13:26:24 +0100

    further work on modal

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,5 @@
 // Get the modal
 var modal = document.getElementById("myModal");
-//let modalTitle = document.getElementById("modal-title")
 
 // Get the button that opens the modal
 var btns = document.querySelectorAll(".plus-btn"); // Get a array of two plus-btn elements
@@ -9,9 +8,11 @@ var btns = document.querySelectorAll(".plus-btn"); // Get a array of two plus-bt
 var span = document.getElementsByClassName("close")[0];
 
 // Get the calendar icon to open modal
-let calendarIconBtn = document.querySelector(".calendar-icon-container")
+let calendarIconBtn = document.querySelectorAll(".calendar-icon-container")
 console.log(calendarIconBtn);
 
+// Toggle remove button
+let removeBtn = document.getElementById("removeButton");
 
 // When the user clicks on the button, open the modal
 btns[0].onclick = function() {
@@ -20,17 +21,19 @@ btns[0].onclick = function() {
 btns[1].onclick = function() {
   modal.style.display = "block";
 }
-
-calendarIconBtn.onclick = function(){
-  modal.style.display = "block"; 
-  let habitData = getHabit(this.id); //send calendarIconBtn's ID the the function in dao to retrieve data
-  document.getElementById("createHabitName").value = habitData.name;
-  document.getElementById("createHabitDescription").value = habitData.description;
-  document.getElementById("downTextIcon").innerHTML = habitData.icon;
-  document.getElementById("dropColor").style.backgroundColor = habitData.color;
+for (let i = 0; i < calendarIconBtn.length; i++){
+calendarIconBtn[i].addEventListener ("click", function(event){
+    modal.style.display = "block"; 
+    let modalTitle = document.getElementById("modal-title");
+    modalTitle.innerHTML = "Change habit"
+    removeBtn.classList.remove("hideBtn")
+    let habitData = getHabit(this.id); //send calendarIconBtn's ID the the function in dao to retrieve data
+    document.getElementById("createHabitName").value = habitData.name;
+    document.getElementById("createHabitDescription").value = habitData.description;
+    document.getElementById("downTextIcon").innerHTML = habitData.icon;
+    document.getElementById("dropColor").style.backgroundColor = habitData.color;
+  }, false);
 }
-
-
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -52,11 +55,14 @@ function addHabit(){
   let dropColor = document.getElementById("dropColor")
   let rbgDropColor = dropColor.style.backgroundColor;
   let createHabitDescription = document.getElementById("createHabitDescription").value;
-  console.log(createHabitName);
-  console.log(dropIcon);
-  console.log(rbgDropColor);
-  console.log(createHabitDescription);
   daoCreateHabit(createHabitName, dropIcon , rbgDropColor, createHabitDescription);
   modal.style.display = "none";
   location.reload();
 }
+
+removeBtn.addEventListener("click", function(){
+  let habitName = document.getElementById("createHabitName").value;
+  removeHabit(habitName);
+  modal.style.display = "none";
+  location.reload();
+})
