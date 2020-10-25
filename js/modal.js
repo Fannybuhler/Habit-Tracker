236768e73a16c4fb3a6cceba3163a4d1e91f7bc8
Fanny Buhlér <fanny.buhler1@gmail.com>
2020-10-25 15:08:36 +0100

    viewing and deleting habits work

--- js/modal.js
+++ js/modal.js
@@ -7,26 +7,39 @@ var btns = document.querySelectorAll(".plus-btn"); // Get a array of two plus-bt
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// Get the calendar icon to open modal
-let calendarIconBtn = document.querySelectorAll(".calendar-icon-container")
-console.log(calendarIconBtn);
-
-// Toggle remove button
-let removeBtn = document.getElementById("removeButton");
-
 // When the user clicks on the button, open the modal
 btns[0].onclick = function() {
   modal.style.display = "block";
 }
+
 btns[1].onclick = function() {
   modal.style.display = "block";
 }
+
+// This aint pretty but it resets the modal "manually"
+function clearInputFields() {
+  document.getElementById("createHabitName").value = "";
+  document.getElementById("createHabitDescription").value = "Description..."
+  document.getElementById("downTextIcon").innerHTML = "Pick an Icon";
+  document.getElementById("dropColor").style.backgroundColor = "";
+  let modalTitle = document.getElementById("modal-title");
+  modalTitle.innerHTML = "Add habit";
+  removeBtn.classList.add("hideBtn");
+  updateBtn.classList.add("hideBtn")
+  addHabitBtn.classList.remove("hideBtn")
+}
+
+// Get every calendar icon to open modal
+let calendarIconBtn = document.querySelectorAll(".calendar-icon-container")
+
 for (let i = 0; i < calendarIconBtn.length; i++){
 calendarIconBtn[i].addEventListener ("click", function(event){
     modal.style.display = "block"; 
     let modalTitle = document.getElementById("modal-title");
     modalTitle.innerHTML = "Change habit"
     removeBtn.classList.remove("hideBtn")
+    updateBtn.classList.remove("hideBtn")
+    addHabitBtn.classList.add("hideBtn")
     let habitData = getHabit(this.id); //send calendarIconBtn's ID the the function in dao to retrieve data
     document.getElementById("createHabitName").value = habitData.name;
     document.getElementById("createHabitDescription").value = habitData.description;
@@ -38,15 +51,18 @@ calendarIconBtn[i].addEventListener ("click", function(event){
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  clearInputFields();
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    clearInputFields();
   }
 }
 
+// Add-button
 var addHabitBtn = document.querySelector(".add-habit-btn");
 addHabitBtn.addEventListener("click", addHabit);
 function addHabit(){
@@ -60,9 +76,21 @@ function addHabit(){
   location.reload();
 }
 
+// Remove-button
+let removeBtn = document.getElementById("removeButton");
 removeBtn.addEventListener("click", function(){
   let habitName = document.getElementById("createHabitName").value;
   removeHabit(habitName);
   modal.style.display = "none";
   location.reload();
 })
+
+// Update button
+let updateBtn = document.getElementById("updateButton");
+updateBtn.addEventListener("click", function(){
+  habitName = document.getElementById("createHabitName").value;
+  let habitData = getHabit(habitName);
+  habitDates = habitData.dates;
+  updateHabit(habitName, habitDates);
+  console.log("Update-knappen fungerar");
+});
