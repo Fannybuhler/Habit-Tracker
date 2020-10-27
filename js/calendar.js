f615ab81c296f0d5debb8d1fce4f80ad1bdbad3c
kol845 <pebo-hamza@hotmail.com>
2020-10-27 14:04:58 +0100

    Calendar changes are applied to home buttons

--- js/calendar.js
+++ js/calendar.js
@@ -152,6 +152,9 @@ function dayClick(){ // Handles the calendar-day clicks.
     }else{
         dates[currentDate].push(id); // Set the day as 'selected' by adding it the the array for the date
     }
+    if(id==(todayDate.getDate()-1)){ // If user clicked on todays date in the calendar
+        toggleHomeBtn(habitName); // Toggle the home-btn for that habit
+    }
     updateHabit(habitName, dates) // Updates tha dates array in the habits localStorage
     renderCalendarHabits()
 }
@@ -166,4 +169,19 @@ function addZero(num){ // Add zero to a number with a single digit (ex: addZero(
     }
     return num;
 } 
+function toggleHomeBtn(habitName){
+    let habitElems = document.querySelectorAll(".card");
+    habitElems.forEach(elem => {
+        if(habitName == elem.id.split("-")[0]){
+            if(elem.classList.contains("is-flipped")){
+                elem.classList.remove("flip-animation");
+                void elem.offsetWidth; // This is a hack to stop the animation reset
+                elem.classList.remove("is-flipped");
+            }else{
+                elem.classList.add('flip-animation');
+                elem.classList.add('is-flipped');
+            }
+        }
+    })
+}
 
--- js/home.js
+++ js/home.js
@@ -2,10 +2,14 @@ function renderHomeButtons(){
     var habitBtnContainer = document.getElementById("habit-btn-container");
     habitBtnContainer.innerHTML = ""; // Empty the innerHTML of container
     var habits = getAllHabits();
+    // console.log("DATE: "+constDateToday)
+    // console.log("DAY: "+(todayDate.getDate()-1))
+
     for(var i = 0; i<habits.length; i++){ // Loop through all habits in localStorage
-        const habit = habits[i];
+        const habit = habits[i];        
         var container = document.createElement('div')
         container.className = "card";
+        container.id = habit.name+"-home-btn"
         var front = document.createElement('div');
         var back = document.createElement('div');
         front.className ="habit-btn habit-btn__face habit-btn__face--front";
@@ -17,7 +21,11 @@ function renderHomeButtons(){
         container.id = habit.name +"-home-btn";
 
         back.innerHTML = '&#10003;<button class="reverse-flip-btn"><svg height="512pt" viewBox="0 0 512 512" width="512pt" xmlns="http://www.w3.org/2000/svg"><path style="fill:#fd0000;" d="m502.121094 1.214844c-5.972656-2.453125-12.863282-1.109375-17.429688 3.476562l-57.597656 57.601563c-47.488281-39.210938-108.417969-62.292969-171.09375-62.292969-141.164062 0-256 114.835938-256 256s114.835938 256 256 256c68.332031 0 132.609375-26.644531 180.96875-75.03125 8.34375-8.339844 8.34375-21.820312 0-30.164062-8.339844-8.339844-21.820312-8.339844-30.164062 0-40.296876 40.320312-93.867188 62.527343-150.804688 62.527343-117.632812 0-213.332031-95.699219-213.332031-213.332031s95.699219-213.332031 213.332031-213.332031c51.414062 0 101.332031 18.496093 140.777344 49.917969l-50.75 50.773437c-4.585938 4.585937-5.929688 11.457031-3.476563 17.429687 2.472657 5.972657 8.296875 9.878907 14.78125 9.878907h138.667969c8.832031 0 16-7.167969 16-16v-138.667969c0-6.484375-3.902344-12.308594-9.878906-14.785156zm0 0"/></svg></button>'
-
+        if(habit.dates[constDateToday] != null){ // Make the btn flipped if today is checked
+            if(habit.dates[constDateToday].includes(""+(todayDate.getDate()-1))){// variable 'todayDate' is from calender.js
+                container.classList.add("is-flipped");
+            }
+        }
         container.appendChild(front);
         container.appendChild(back);
 
@@ -27,19 +35,20 @@ function renderHomeButtons(){
 }
 renderHomeButtons();
 
-// All buttons that are flippable
+// All buttons that are flippable(habit buttons)
 var flipBtns = document.querySelectorAll('.habit-btn__face--front');
 flipBtns.forEach(elem =>{
-    elem.addEventListener('click', addFlipp);
+    elem.addEventListener('click', habitChecked);
 })
-function addFlipp(){
-    if(!this.parentNode.classList.contains("flip-animation")){
-        this.parentNode.classList.add('flip-animation');
-        this.parentNode.classList.add('is-flipped');
+function habitChecked(){
+    
+    addFlipp(this);
+}
+function addFlipp(elem){
+    if(!elem.parentNode.classList.contains("flip-animation")){
+        elem.parentNode.classList.add('flip-animation');
+        elem.parentNode.classList.add('is-flipped');
     }
-    // if(!this.parentNode.classList.contains("is-flipped")){
-    //     this.parentNode.classList.add('is-flipped');
-    // }
 }
 // Reverse buttons
 var revBtns = document.querySelectorAll('.reverse-flip-btn');
@@ -48,28 +57,6 @@ revBtns.forEach(elem =>{
 })
 function reverseFlip(){
     this.parentNode.parentNode.classList.remove("flip-animation");
-    void this.parentNode.parentNode.offsetWidth;
+    void this.parentNode.parentNode.offsetWidth; // This is a hack to stop the animation reset
     this.parentNode.parentNode.classList.remove("is-flipped");
-
-    // if(this.parentNode.parentNode.classList.contains("flip-animation")){
-    //     this.parentNode.parentNode.classList.remove("flip-animation");
-    //     this.parentNode.parentNode.classList.remove("rev-flipp");
-    //     void this.parentNode.parentNode.offsetWidth;
-    //     this.parentNode.parentNode.classList.add("rev-flipp");
-
-    // }else if (this.parentNode.parentNode.classList.contains("is-flipped")){
-    //     this.parentNode.parentNode.classList.remove("is-flipped");
-    //     this.parentNode.parentNode.classList.add("rev-flipp");
-    // }else{
-    //     this.parentNode.parentNode.classList.add("flip-animation");
-    // }
-    
-}
-// function reload(elem){
-//     var content = elem.innerHTML;
-//     elem.innerHTML= content; 
-// }
-function homeBtnClick(){
-    console.log("FLIPPIN")
-    this.classList.toggle('is-flipped')
 }
