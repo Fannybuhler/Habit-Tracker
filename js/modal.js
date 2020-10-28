575128f8df9ddbd7da1d03f29826c26b186f951c
Fanny Buhlér <fanny.buhler1@gmail.com>
2020-10-28 14:29:29 +0100

    Form validation

--- js/modal.js
+++ js/modal.js
@@ -53,14 +53,13 @@ addHabitBtn.addEventListener("click", addHabit);
 let updateBtn = document.getElementById("updateButton");
 updateBtn.addEventListener("click", addHabit);
 
-// Form validation
+// Name validation
 const nameValidation = document.querySelector(".text-input");
 
 function addHabit(){
   let createHabitName = document.getElementById("createHabitName").value;
-
   let dropIconContainer = document.getElementById("dropIcon")
-  let dropIcon = dropIconContainer.getElementsByTagName("svg")[0].outerHTML;
+  let dropIcon = document.getElementById("dropIcon").getElementsByTagName("svg")[0].outerHTML;
 
   let dropColor = document.getElementById("dropColor")
   let rbgDropColor = dropColor.style.backgroundColor;
@@ -72,15 +71,31 @@ function addHabit(){
     let habitData = getHabit(createHabitName);
     dates = habitData.dates
   }
-  
-  if (nameValidation.value.length === 0){
-    nameValidation.style.border = "0.5px solid #28BFCF";
-  } else if (dropColor.style.backgroundColor == "") {
-    dropColor.style.border  = "0.5px solid #28BFCF";
+
+  if (nameValidation.value.length === 0 || dropIcon.includes("actualArrow") || !rbgDropColor){
+      if(nameValidation.value.length===0){
+        nameValidation.style.border = "0.5px solid #28BFCF";
+      }else{
+        nameValidation.style.border = "";
+      }
+      if(dropIcon.includes("actualArrow")){
+        dropIconContainer.style.border  = "0.5px solid #28BFCF";
+      }else{
+        dropIconContainer.style.border  = ""
+      }
+      if(!rbgDropColor){
+        dropColor.style.border  = "0.5px solid #28BFCF";
+      }else{
+        dropColor.style.border  = "";
+      }
+
   } else {
-    daoCreateHabit(createHabitName, dropIcon , rbgDropColor, createHabitDescription, dates);
-    modal.style.display = "none";
-    location.reload();
+      daoCreateHabit(createHabitName, dropIcon , rbgDropColor, createHabitDescription, dates);
+      modal.style.display = "none";
+      dropColor.style.border  = "";
+      dropIconContainer.style.border  = "";
+      nameValidation.style.border = "";
+      location.reload();
   }
 }
 
